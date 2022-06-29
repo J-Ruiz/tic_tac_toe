2b00f0afd7c1ec76fde6d4edd9eddc339ba984a5
Jake Ruiz <jwr318@gmail.com>
2022-06-29 16:43:07 -0400

    updated the css color palette. created the tic tac toe object in the CSS to privatize the functions within the tic tac toe object

--- script.js
+++ script.js
@@ -1,4 +1,173 @@
+const ticTacToe = () => {
+    let gameboard =  ["","","","","","","","",""];
+    
+    let whoseTurnIsIt = "1";
 
+    let player1Score = 0;
+
+    let player2Score = 0;
+
+    let numberOfClicks = 0;
+    
+    let updateScore = (winningPlayer) => {
+        // if winning player is player1
+    if(winningPlayer == "1"){
+        player1Score++;
+        document.getElementById("P1-score").innerHTML = player1Score;
+    }
+        
+    // else
+    else{
+        player2Score++;
+        document.getElementById("P2-score").innerHTML = player2Score;
+    }
+};
+
+let clearGameBoard = () => {
+    // while (#gameboard.firstChild){
+    //  #gameboard.removeChild(#gameboard.firstChild)
+    // }
+    
+    document.getElementById("gameboard").innerHTML= "";
+    gameboard = ["","","","","","","","",""];
+}
+
+
+let gameWin = () => {
+    //if (there are 3 in a row horizontally, vertically or diagonally)
+    let topRow = [gameboard[0],gameboard[1],gameboard[2]];
+    let middleRow = [gameboard[3],gameboard[4],gameboard[5]];
+    let bottomRow = [gameboard[6],gameboard[7],gameboard[8]];
+    let leftColumn = [gameboard[0],gameboard[3],gameboard[6]];
+    let middleColumn = [gameboard[1],gameboard[4],gameboard[7]];
+    let rightColumn = [gameboard[2],gameboard[5],gameboard[8]];
+    let Diagonal1 = [gameboard[0],gameboard[4],gameboard[8]];
+    let Diagonal2 = [gameboard[2],gameboard[4],gameboard[6]];
+
+    let winningArray = [topRow,middleRow,bottomRow,leftColumn,middleColumn,rightColumn,Diagonal1,Diagonal2]
+
+    for(let i = 0; i<winningArray.length; i++){
+        let possibleWin = [];
+        for(let j = 0; j<winningArray[i].length; j++){
+            possibleWin.push(winningArray[i][j]);
+            
+        }
+
+        function xTest(value){
+            return value=="X";
+        }
+
+        function oTest(value){
+            return value=="O";
+        }
+
+        console.log("array with every value" , possibleWin,possibleWin.every(xTest));
+
+        if(possibleWin.every(xTest)){
+            alert("player 1 wins!")
+            updateScore("1");
+            clearGameBoard();
+            playerTurnSwitcher()
+            numberOfClicks=0;
+            createGameboard();
+            return;
+        }
+
+        else if(possibleWin.every(oTest)){
+            alert("player 2 wins!")
+            updateScore("2");
+            clearGameBoard();
+            numberOfClicks=0;
+            createGameboard();
+            return;
+        }
+ 
+    }
+    if(numberOfClicks == 9){
+        alert("Tie Game! Try Again")
+        playerTurnSwitcher();
+        clearGameBoard();
+        numberOfClicks=0;
+        createGameboard();
+        return;
+    }
+    return;
+};
+
+let playerTurnSwitcher = () => {
+    //if it was player 1's turn
+    // make inner HTML = Player 2's turn
+    // update variable whoseTurnIsIt="2"
+    if(whoseTurnIsIt == "1"){
+        document.getElementById("turn-marker").innerHTML = "Player 2's Turn"
+        whoseTurnIsIt = "2"
+    }
+    
+    //else 
+        // make inner HTML = Player 1's turn
+        // update variable whoseTurnIsIt="1"
+    else{
+        document.getElementById("turn-marker").innerHTML = "Player 1's Turn"
+        whoseTurnIsIt = "1"
+    }
+    
+};
+
+let addIconToGameboard = (clickedElement) => {
+    // if player 1's turn and player 1 clicks gameboard box 
+        
+    if(whoseTurnIsIt == "1"){
+            // array gameboard[clickedElement.target.id] = X.
+        if(gameboard[Number(clickedElement.target.id)] == ""){
+            gameboard[Number(clickedElement.target.id)] = "X";
+            // add X to innerHTML of clicked element
+            clickedElement.target.innerHTML = gameboard[Number(clickedElement.target.id)]
+                // execute the playerTurnSwitcher() function
+            numberOfClicks++;
+            gameWin();
+            playerTurnSwitcher();
+        }
+    }
+        
+    // else 
+    else if (gameboard[Number(clickedElement.target.id)] == ""){
+            // array gameboard[clickedElement.target.id] = O.
+        gameboard[Number(clickedElement.target.id)] = "O";
+            // add O to innerHTML of clicked element
+        clickedElement.target.innerHTML = gameboard[Number(clickedElement.target.id)]
+            // execute the playerTurnSwitcher function
+        numberOfClicks++;
+        gameWin();
+        playerTurnSwitcher();
+    }
+        
+};
+
+let createGameboard = () => {
+    //create gameboard boxes from array
+    for(let i = 0; i<gameboard.length; i++){
+        let newBox = document.createElement("div");
+        newBox.setAttribute("class", "gamebox")
+        newBox.setAttribute("id", `${i}`)
+        newBox.innerHTML = gameboard[i];
+
+
+        newBox.addEventListener("click", function listener(event){
+            //execute addIconToGameBoard(event) Function with event data as a parameter
+            addIconToGameboard(event);
+            //remove eventListener
+            //newBox.removeEventListener("click", listener)
+        })
+        document.getElementById("gameboard").appendChild(newBox);
+    }
+};
+
+createGameboard();
+
+}
+ticTacToe();
+
+/*
 let gameboard = ["","","","","","","","",""];
 
 let whoseTurnIsIt = "1";
@@ -163,3 +332,4 @@ let createGameboard = () => {
 };
 
 createGameboard();
+*/
